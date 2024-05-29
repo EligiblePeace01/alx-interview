16c5e9334fcf9afd2d805d21c6f17dcd162fffa7
EligiblePeace01 <enwerempeace@gmail.com>
2024-05-29 14:17:59 -0700

    make file executable

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,53 +1,91 @@
 #!/usr/bin/python3
-"""The N queens puzzle is the challenge of placing N non-attacking queens
-on an NxN chessboard. Write a program that solves the N queens problem.
-"""
-from sys import argv
+'''N-Queens Challenge'''
 
-if __name__ == "__main__":
-    if len(argv) != 2:
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
         exit(1)
-    n = argv[1]
-    if n.isnumeric():
-        n = int(n)
-    else:
-        print("N must be a number")
-        exit(1)
+
     if n < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         exit(1)
 
-    cols = set()
-    posDia = set()  # (r + c)
-    negDia = set()  # (r - c)
+    solutions = []
+    placed_queens = []  # coordinates format [row, column]
+    stop = False
+    r = 0
+    c = 0
 
-    results = []
-    res = []
+    # iterate thru rows
+    while r < n:
+        goback = False
+        # iterate thru columns
+        while c < n:
+            # check is current column is safe
+            safe = True
+            for cord in placed_queens:
+                col = cord[1]
+                if(col == c or col + (r-cord[0]) == c or
+                        col - (r-cord[0]) == c):
+                    safe = False
+                    break
 
-    def backtrack(r):
-        """function"""
-        if r == n:
-            results.append(res.copy())
-            return
-
-        for c in range(n):
-            if c in cols or (c + r) in posDia or (r - c) in negDia:
+            if not safe:
+                if c == n - 1:
+                    goback = True
+                    break
+                c += 1
                 continue
 
-            cols.add(c)
-            posDia.add(c + r)
-            negDia.add(r - c)
-            res.append([r, c])
+            # place queen
+            cords = [r, c]
+            placed_queens.append(cords)
+            # if last row, append solution and reset all to last unfinished row
+            # and last safe column in that row
+            if r == n - 1:
+                solutions.append(placed_queens[:])
+                for cord in placed_queens:
+                    if cord[1] < n - 1:
+                        r = cord[0]
+                        c = cord[1]
+                for i in range(n - r):
+                    placed_queens.pop()
+                if r == n - 1 and c == n - 1:
+                    placed_queens = []
+                    stop = True
+                r -= 1
+                c += 1
+            else:
+                c = 0
+            break
+        if stop:
+            break
+        # on fail: go back to previous row
+        # and continue from last safe column + 1
+        if goback:
+            r -= 1
+            while r >= 0:
+                c = placed_queens[r][1] + 1
+                del placed_queens[r]  # delete previous queen coordinates
+                if c < n:
+                    break
+                r -= 1
+            if r < 0:
+                break
+            continue
+        r += 1
 
-            backtrack(r + 1)
-
-            cols.remove(c)
-            posDia.remove(c + r)
-            negDia.remove(r - c)
-            res.remove([r, c])
-
-    backtrack(0)
-
-    for one in results:
-        print(one)
+    for idx, val in enumerate(solutions):
+        if idx == len(solutions) - 1:
+            print(val, end='')
+        else:
+            print(val)
